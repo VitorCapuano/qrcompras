e4ebc10370fa179c837a52d5d62b1d684b8110a9
VitorCapuano <capuano.vitor@gmail.com>
2017-09-14 20:46:26 -0300

    Controller produtos sem logica

---
+++ QrCompras/QrCompras/Controllers/ProdutoController.cs
@@ -0,0 +1,58 @@
+﻿using QRCompras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QrCompras.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(Produto produto)
+        {
+            try
+            {
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+        [HttpPost]
+        public ActionResult Editar(int id)
+        {
+            try
+            { 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
